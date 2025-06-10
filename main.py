470dc7af51a228ca7874dfe167fe7b4db748dd57
dikramov0803 <dikramov08@gmail.com>
2025-06-10 10:29:35 +0500

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 # main.py
-print("Hello, world!")
+
+import asyncio
+from flask import Flask, request
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from config import BOT_TOKEN, WEBHOOK_URL
+from database import init_db
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+async def webhook():
+    data = request.get_json(force=True)
+    update = Update.de_json(data, application.bot)
+    await application.process_update(update)
+    return "ok"
+
+# Команда /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! 👋 Выберите язык.")
+
+if __name__ == "__main__":
+    init_db()
+
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+
+    # Включаем webhook
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=8080,
+        webhook_url=WEBHOOK_URL
+    )
