24599a750baeb4dde0c20d5615019de53e036c3e
dikramov0803 <dikramov08@gmail.com>
2025-06-10 10:28:30 +0500

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+# config.py
 
+# Вставь сюда токен своего бота (от BotFather)
+BOT_TOKEN = "8134914263:AAGbkV4iRPdfSlaCMJF_-UpSytk8KzlxG1c"
+
+# Укажи Telegram ID админа (твой ID, чтобы открыть доступ к админке)
+ADMIN_ID = 7964176983
+
+# Railway URL (мы вставим его позже, когда получим адрес)
+WEBHOOK_URL = "https://твоя-ссылка-на-railway.up.railway.app"
