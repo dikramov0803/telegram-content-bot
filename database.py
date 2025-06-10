c8fb05640bf573459f6632acf3764458cf368411
dikramov0803 <dikramov08@gmail.com>
2025-06-10 10:29:01 +0500

    database.py

--- database.py
+++ database.py
@@ -1,1 +1,37 @@
+# database.py
 
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("bot.db")
+    cursor = conn.cursor()
+
+    # Таблицы: разделы, темы, файлы
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS sections (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name_ru TEXT,
+        name_uz TEXT
+    )""")
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS topics (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        section_id INTEGER,
+        name_ru TEXT,
+        name_uz TEXT,
+        FOREIGN KEY (section_id) REFERENCES sections(id)
+    )""")
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS files (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        topic_id INTEGER,
+        file_id TEXT,
+        file_name TEXT,
+        file_type TEXT,
+        FOREIGN KEY (topic_id) REFERENCES topics(id)
+    )""")
+
+    conn.commit()
+    conn.close()
